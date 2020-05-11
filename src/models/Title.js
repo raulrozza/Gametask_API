a1928bde81bf6ee1672d2cd5a2257d853d423ead
Raul Rosá <raul.hmetal@gmail.com>
2020-05-11 10:52:25 -0300

    New feature: Added title model. Closes #6

---
+++ src/models/Title.js
@@ -0,0 +1,11 @@
+const { Schema, model} = require('mongoose');
+
+const TitleSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+}, {
+});
+
+module.exports = model('Title', TitleSchema);
