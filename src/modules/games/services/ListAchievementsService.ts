47fd7698fe7ce73c0f0f8cfa85570a2690a126dc
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-02-08 21:39:35 -0300

    Added list achievement service

---
+++ src/modules/games/services/ListAchievementsService.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import { IAchievementsRepository } from '@modules/games/repositories';
+import { IAchievement } from '@modules/games/entities';
+
+@injectable()
+export default class ListGamesService {
+  constructor(
+    @inject('AchievementsRepository')
+    private achievementsRepository: IAchievementsRepository,
+  ) {}
+
+  public async execute(gameId: string): Promise<IAchievement[]> {
+    return await this.achievementsRepository.findAllFromGame(gameId);
+  }
+}
