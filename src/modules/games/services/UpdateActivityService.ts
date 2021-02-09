b5e6067558db437c2f3538253592178c88ff0a7a
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-02-09 11:32:37 -0300

    Added update activity service

---
+++ src/modules/games/services/UpdateActivityService.ts
@@ -0,0 +1,117 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import errorCodes from '@config/errorCodes';
+import { RequestError } from '@shared/errors/implementations';
+
+import IUpdateActivityDTO from '@modules/games/dtos/IUpdateActivityDTO';
+import { IActivity, IActivityLog } from '@modules/games/entities';
+import { IActivitiesRepository } from '@modules/games/repositories';
+
+interface INewActivityDTO {
+  name: string;
+  experience: number;
+  description?: string;
+  dmRules?: string;
+}
+
+@injectable()
+export default class UpdateActivityService {
+  private originalActivity: IActivity;
+
+  constructor(
+    @inject('ActivitiesRepository')
+    private activitiesRepository: IActivitiesRepository,
+  ) {
+    this.originalActivity = {} as IActivity;
+  }
+
+  public async execute({
+    gameId,
+    userId,
+    id,
+    name,
+    experience,
+    description,
+    dmRules,
+  }: IUpdateActivityDTO): Promise<IActivity> {
+    const foundActivity = await this.findOriginalActivity(id, gameId);
+
+    if (!foundActivity)
+      throw new RequestError(
+        'This activity does not exist',
+        errorCodes.RESOURCE_NOT_FOUND,
+        400,
+      );
+
+    const newActivityLog = this.generateActivityLog(userId, {
+      name,
+      description,
+      dmRules,
+      experience,
+    });
+
+    const updatedActivity = {
+      id,
+      name,
+      description,
+      dmRules,
+      experience,
+      game: gameId,
+      changelog: [newActivityLog, ...this.originalActivity.changelog],
+    };
+
+    await this.activitiesRepository.update(updatedActivity);
+
+    return updatedActivity;
+  }
+
+  private async findOriginalActivity(
+    activityId: string,
+    gameId: string,
+  ): Promise<boolean> {
+    const activity = await this.activitiesRepository.findOne(
+      activityId,
+      gameId,
+    );
+
+    if (activity) this.originalActivity = activity;
+
+    return Boolean(activity);
+  }
+
+  private generateActivityLog(
+    userId: string,
+    newActivity: INewActivityDTO,
+  ): IActivityLog {
+    const changes = this.getChanges(newActivity);
+
+    const logVersion = this.originalActivity.changelog[0]
+      ? this.originalActivity.changelog[0].version + 1
+      : 1;
+
+    const activityLog = {
+      version: logVersion,
+      log: new Date(),
+      changes,
+      userId,
+    };
+
+    return activityLog;
+  }
+
+  private getChanges(newActivity: INewActivityDTO): Partial<IActivity> {
+    const changes: Partial<IActivity> = {};
+
+    if (newActivity.name !== this.originalActivity.name)
+      changes.name = newActivity.name;
+    if (newActivity.description !== this.originalActivity.description)
+      changes.description = newActivity.description;
+    if (newActivity.experience !== this.originalActivity.experience)
+      changes.experience = newActivity.experience;
+    if (newActivity.dmRules !== this.originalActivity.dmRules)
+      changes.dmRules = newActivity.dmRules;
+
+    return changes;
+  }
+}
