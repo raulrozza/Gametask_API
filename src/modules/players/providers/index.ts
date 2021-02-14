25f4f93221d73360c423d64ebf39959939c34c42
Raul Mendes Rosá <raul.hmetal@gmail.com>
2021-02-14 09:31:59 -0300

    Added new repositories to provider

--- src/modules/players/providers/index.ts
+++ src/modules/players/providers/index.ts
@@ -1,9 +1,27 @@
 import { container } from 'tsyringe';
 
-import { PlayersRepository } from '@modules/players/infra/mongoose/repositories';
-import { IPlayersRepository } from '@modules/players/repositories';
+import {
+  CompleteActivityRequestRepository,
+  PlayersRepository,
+  UnlockAchievementRequestRepository,
+} from '@modules/players/infra/mongoose/repositories';
+import {
+  ICompleteActivityRequestRepository,
+  IPlayersRepository,
+  IUnlockAchievementRequestRepository,
+} from '@modules/players/repositories';
 
 container.registerSingleton<IPlayersRepository>(
   'PlayersRepository',
   PlayersRepository,
+);
+
+container.registerSingleton<IUnlockAchievementRequestRepository>(
+  'UnlockAchievementRequestRepository',
+  UnlockAchievementRequestRepository,
+);
+
+container.registerSingleton<ICompleteActivityRequestRepository>(
+  'CompleteActivityRequestRepository',
+  CompleteActivityRequestRepository,
 );
