ad6fbfcf73a488d0f7b8b437e230750d8963352b
Raul <raul.hmetal@gmail.com>
2021-03-05 15:40:11 -0300

    Fix: Returning id field on title

--- src/modules/games/infra/mongoose/entities/Title.ts
+++ src/modules/games/infra/mongoose/entities/Title.ts
@@ -11,19 +11,18 @@ export interface ITitlePopulatedDocument extends ITitleDocument {
   game: IGamePopulatedDocument;
 }
 
-const TitleSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    game: {
-      type: Schema.Types.ObjectId,
-      ref: 'Game',
-      required: true,
-    },
+const TitleSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {},
-);
+  game: {
+    type: Schema.Types.ObjectId,
+    ref: 'Game',
+    required: true,
+  },
+});
+
+TitleSchema.set('toJSON', { virtuals: true });
 
 export default model<ITitleDocument>('Title', TitleSchema);
